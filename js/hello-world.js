789aadd5cda6682b3340c571bb9919d3b5a1304c
Vaishar Antonín <vaishar@lib.vutbr.cz>
2018-02-08 12:53:56 +0100

    qoutes

--- js/hello-world.js
+++ js/hello-world.js
@@ -1,4 +1,4 @@
  app.component('prmLogoAfter', {
 		bindings: { },
-        template: `<div class='hello-world'><span>Hello World</span></div>`
+        template: `<div class="hello-world"><span>Hello World</span></div>`
  });
